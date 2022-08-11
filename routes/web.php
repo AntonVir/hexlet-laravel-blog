8bac0cbb95b92df855cf90b451169415732611ca
a.vishniakov <avforbiz@gmail.com>
2022-08-11 22:10:00 +0800

    laravel lesson #4

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('about', function () {
+    $tags = ['обучение', 'программирование', 'php', 'oop'];
+    return view('about', [
+        'tags' => $tags
+    ]);
 });
